f698e66e613a042c608ed7b7e2ced65dba6ec97a
Valencia Caldwell <VKCaldwell@gmail.com>
2021-04-04 19:34:56 -0500

    updated

---
+++ lib/march_madness/scraper.rb
@@ -0,0 +1,16 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+class Scraper 
+    attr_accessor :team
+
+    @@bracket = []
+
+
+    def initialize 
+        puts "I am in the scraper!"
+    end
+
+
+end
